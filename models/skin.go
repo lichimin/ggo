3b509999ba9122a62176d6ea0a166cf5a73c3ed0
WIN-OE07V8CF6N8\EDY <295207418@qq.com>
2025-12-03 14:12:13 +0800

    no message

--- models/skin.go
+++ models/skin.go
@@ -2,17 +2,17 @@ package models
 
 type Skin struct {
 	ID              uint     `json:"id" gorm:"primarykey"`
-	Name            string   `json:"name" gorm:"size:100;not null"`      // 皮肤名称
-	Attack          int      `json:"attack" gorm:"default:0"`            // 攻击力
-	HP              int      `json:"hp" gorm:"default:0"`                // 血量
-	AtkType         int      `json:"atk_type" gorm:"default:0"`          // 攻击类型 1穿透  2散射 3反弹
-	AtkSpeed        int      `json:"atk_speed" gorm:"default:0"`         // 攻击速度
-	CriticalRate    float64  `json:"critical_rate" gorm:"default:0.0"`   // 暴击率 (0-1)
-	CriticalDamage  float64  `json:"critical_damage" gorm:"default:1.5"` // 暴击伤害 (倍数)
-	BackgroundURL   string   `json:"background_url" gorm:"size:500"`     // 背景图片地址
-	IdleImageURLs   []string `json:"idle_image_urls" gorm:"type:json"`   // 待机图片（JSON格式多个图片）
-	AttackImageURLs []string `json:"attack_image_urls" gorm:"type:json"` // 攻击图片（JSON格式多个图片）
-	MoveImageURLs   []string `json:"move_image_urls" gorm:"type:json"`   // 移动图片（JSON格式多个图片）
-	CreatedAt       int64    `json:"created_at" gorm:"autoCreateTime"`   // 创建时间
-	UpdatedAt       int64    `json:"updated_at" gorm:"autoUpdateTime"`   // 更新时间
+	Name            string   `json:"name" gorm:"size:100;not null"`                      // 皮肤名称
+	Attack          int      `json:"attack" gorm:"default:0"`                            // 攻击力
+	HP              int      `json:"hp" gorm:"default:0"`                                // 血量
+	AtkType         int      `json:"atk_type" gorm:"default:0"`                          // 攻击类型 1穿透  2散射 3反弹
+	AtkSpeed        int      `json:"atk_speed" gorm:"default:0"`                         // 攻击速度
+	CriticalRate    float64  `json:"critical_rate" gorm:"default:0.0"`                   // 暴击率 (0-1)
+	CriticalDamage  float64  `json:"critical_damage" gorm:"default:1.5"`                 // 暴击伤害 (倍数)
+	BackgroundURL   string   `json:"background_url" gorm:"size:500"`                     // 背景图片地址
+	IdleImageURLs   []string `json:"idle_image_urls" gorm:"type:json;serializer:json"`   // 待机图片（JSON格式多个图片）
+	AttackImageURLs []string `json:"attack_image_urls" gorm:"type:json;serializer:json"` // 攻击图片（JSON格式多个图片）
+	MoveImageURLs   []string `json:"move_image_urls" gorm:"type:json;serializer:json"`   // 移动图片（JSON格式多个图片）
+	CreatedAt       int64    `json:"created_at" gorm:"autoCreateTime"`                   // 创建时间
+	UpdatedAt       int64    `json:"updated_at" gorm:"autoUpdateTime"`                   // 更新时间
 }
