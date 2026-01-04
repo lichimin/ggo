f51ff5764bfc9c2fd5d5d88ea09d7c828d10271d
WIN-OE07V8CF6N8\EDY <295207418@qq.com>
2026-01-04 17:02:21 +0800

    no message

--- models/area.go
+++ models/area.go
@@ -2,9 +2,9 @@ package models
 
 // Area 区服模型
 type Area struct {
-	ID    uint `json:"id" gorm:"primarykey"`
-	Area  int  `json:"area" gorm:"size:50"`                  // 区服编号
-	IsNew bool `json:"is_new" gorm:"not null;default:false"` // 是否新服
+	ID    uint   `json:"id" gorm:"primarykey"`
+	Area  string `json:"area" gorm:"size:50"`                  // 区服编号
+	IsNew bool   `json:"is_new" gorm:"not null;default:false"` // 是否新服
 }
 
 // TableName 指定表名
