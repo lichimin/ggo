1d270e9163a5596f7290a091129379b66194e68b
WIN-OE07V8CF6N8\EDY <295207418@qq.com>
2026-01-04 16:57:16 +0800

    no message

--- models/area.go
+++ models/area.go
@@ -2,12 +2,9 @@ package models
 
 // Area 区服模型
 type Area struct {
-	ID       uint   `json:"id" gorm:"primarykey"`
-	Area     int    `json:"area" gorm:"not null;unique;index:idx_area"` // 区服编号
-	IsNew    bool   `json:"is_new" gorm:"not null;default:false"`       // 是否新服
-	Status   int    `json:"status" gorm:"not null;default:1"`           // 区服状态 1:正常 2:维护 3:爆满
-	Name     string `json:"name" gorm:"size:50"`                        // 区服名称
-	MaxUsers int    `json:"max_users" gorm:"not null;default:1000"`     // 最大用户数
+	ID    uint `json:"id" gorm:"primarykey"`
+	Area  int  `json:"area" gorm:"size:50"`                  // 区服编号
+	IsNew bool `json:"is_new" gorm:"not null;default:false"` // 是否新服
 }
 
 // TableName 指定表名
